929244bd06baa80340086c2bee374c339df180ea
HP <HP@DESKTOP-PA1TA8P>
2023-11-08 00:53:21 +0530

    commit

---
+++ Mad/src/Jamcehttu.java
@@ -0,0 +1,22 @@
+import java.util.*;
+public class Jamcehttu {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		  int x,y,a,b,z;
+		  Scanner sc=new Scanner(System.in);
+		  System.out.println("enter the x:");
+		  x=sc.nextInt();
+		  System.out.println("enter the y:");
+		  y=sc.nextInt();
+		  a=x+y;
+		  b=y-x;
+		  z=x-z;
+		  System.out.println("x="+x);
+		  System.out.println("y="+y);
+		  System.out.println("a="+a);
+		  System.out.println("b="+b);
+
+	}
+
+}
